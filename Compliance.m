3f12d82728660ba991ed20defdb5ffed45442072
Francisco-24 <francisco.d.alves@tecnico.ulisboa.pt>
2023-12-27 16:18:14 +0000

    Vamo dale porra

--- Compliance.m
+++ Compliance.m
@@ -3,7 +3,11 @@ clear all
 close all
 
 %% Condição Fronteira Tensão XX 
-[energy, energy_density, extensaoXX, extensaoYY, extensaoZZ, stressesXX] = leitura_excel();
+
+syms Ex Ey Ex vxy vyx vxz vzx vyz vzy
+
+%%%%%%%%%%% sigma xx aplicado %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+[energy, energy_density, extensaoXX, extensaoYY, extensaoZZ, stressesXX] = leitura_excel(1);
 
 element_volume = energy./energy_density;
 Volume_RVE = sum(element_volume);
@@ -12,10 +16,6 @@ extensaoXX_med = sum(extensaoXX.*element_volume)/Volume_RVE;
 extensaoYY_med = sum(extensaoYY.*element_volume)/Volume_RVE;
 extensaoZZ_med = sum(extensaoZZ.*element_volume)/Volume_RVE;
 stressesXX_med = sum(stressesXX.*element_volume)/Volume_RVE;
-
-syms Ex Ey Ex vxy vyx vxz vzx vyz vzy
-
-% sigma xx aplicado
 
 sigma_xx = 1;
 
@@ -27,6 +27,61 @@ equations = [eq1, eq2, eq3];
 solution = solve(equations, [Ex,vxy,vxz]);
 disp(solution);
 
-C11 = energy_RVE*2/Volume_RVE
-Exx_energy = 1/C11*10^-3
+Ex = double(solution.Ex);
+vxy = double(solution.vxy);
+vxz = double(solution.vxz);
+
+%%%%%%%%%%% sigma yy aplicado %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[energy, energy_density, extensaoXX, extensaoYY, extensaoZZ, stressesYY] = leitura_excel(2);
+
+element_volume = energy./energy_density;
+Volume_RVE = sum(element_volume);
+energy_RVE = sum(energy);
+extensaoXX_med = sum(extensaoXX.*element_volume)/Volume_RVE;
+extensaoYY_med = sum(extensaoYY.*element_volume)/Volume_RVE;
+extensaoZZ_med = sum(extensaoZZ.*element_volume)/Volume_RVE;
+stressesYY_med = sum(stressesYY.*element_volume)/Volume_RVE;
+
+sigma_yy = 1;
+
+eq1 = extensaoXX_med == -sigma_yy*vyx/Ey;
+eq2 = extensaoYY_med == sigma_yy/Ey;
+eq3 = extensaoZZ_med == -sigma_yy*vyz/Ey;
+
+equations = [eq1, eq2, eq3];
+solution = solve(equations, [Ex,vxy,vxz]);
+disp(solution);
+
+Ey = double(solution.Ey);
+vyx = double(solution.vyx);
+vyz = double(solution.vyz);
+
+%%%%%%%%%%% sigma zz aplicado %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% [energy, energy_density, extensaoXX, extensaoYY, extensaoZZ, stressesZZ] = leitura_excel(3);
+% 
+% element_volume = energy./energy_density;
+% Volume_RVE = sum(element_volume);
+% energy_RVE = sum(energy);
+% extensaoXX_med = sum(extensaoXX.*element_volume)/Volume_RVE;
+% extensaoYY_med = sum(extensaoYY.*element_volume)/Volume_RVE;
+% extensaoZZ_med = sum(extensaoZZ.*element_volume)/Volume_RVE;
+% stressesZZ_med = sum(stressesZZ.*element_volume)/Volume_RVE;
+% 
+% sigma_zz = 1;
+% 
+% eq1 = extensaoXX_med == -sigma_zz*vzx/Ez;
+% eq2 = extensaoYY_med == -sigma_zz*vzy/Ez;
+% eq3 = extensaoZZ_med == sigma_zz/Ez;
+% 
+% equations = [eq1, eq2, eq3];
+% solution = solve(equations, [Ex,vxy,vxz]);
+% disp(solution);
+% 
+% Ez = double(solution.Ez);
+% vzx = double(solution.vzx);
+% vzy = double(solution.vzy);
+
+
+% C11 = energy_RVE*2/Volume_RVE
+% Exx_energy = 1/C11*10^-3
 
--- leitura_excel.m
+++ leitura_excel.m
@@ -1,17 +1,46 @@
-function [energy, energy_density, extensaoXX, extensaoYY, extensaoZZ, stressesXX] = leitura_excel()
+function [energy, energy_density, extensaoXX, extensaoYY, extensaoZZ, stressesAA] = leitura_excel(n)
     
+if n==1
     energy_tab = readtable('FEM_Decente/TensaoX/energy.csv');
     energy_density_tab = readtable('FEM_Decente/TensaoX/energy_density.csv');
     extensaoXX_tab = readtable('FEM_Decente/TensaoX/extensaoXX.csv');
     extensaoYY_tab = readtable('FEM_Decente/TensaoX/extensaoYY.csv');
     extensaoZZ_tab = readtable('FEM_Decente/TensaoX/extensaoZZ.csv');
-    stressesXX_tab = readtable('FEM_Decente/TensaoX/stressesXX.csv');
+    stressesAA_tab = readtable('FEM_Decente/TensaoX/stressesXX.csv');
 
     energy = table2array(energy_tab(:,2));
     energy_density = table2array(energy_density_tab(:,2));
     extensaoXX = table2array(extensaoXX_tab(:,2));
     extensaoYY = table2array(extensaoYY_tab(:,2));
     extensaoZZ = table2array(extensaoZZ_tab(:,2));
-    stressesXX = table2array(stressesXX_tab(:,2));
+    stressesAA = table2array(stressesAA_tab(:,2));
+elseif n==2
+    energy_tab = readtable('FEM_Decente/TensaoY/energy.csv');
+    energy_density_tab = readtable('FEM_Decente/TensaoY/energy_density.csv');
+    extensaoXX_tab = readtable('FEM_Decente/TensaoY/extensaoXX.csv');
+    extensaoYY_tab = readtable('FEM_Decente/TensaoY/extensaoYY.csv');
+    extensaoZZ_tab = readtable('FEM_Decente/TensaoY/extensaoZZ.csv');
+    stressesAA_tab = readtable('FEM_Decente/TensaoY/stressesAA.csv');
+
+    energy = table2array(energy_tab(:,2));
+    energy_density = table2array(energy_density_tab(:,2));
+    extensaoXX = table2array(extensaoXX_tab(:,2));
+    extensaoYY = table2array(extensaoYY_tab(:,2));
+    extensaoZZ = table2array(extensaoZZ_tab(:,2));
+    stressesAA = table2array(stressesAA_tab(:,2));
+elseif n==3
+    energy_tab = readtable('FEM_Decente/TensaoZ/energy.csv');
+    energy_density_tab = readtable('FEM_Decente/TensaoZ/energy_density.csv');
+    extensaoXX_tab = readtable('FEM_Decente/TensaoZ/extensaoXX.csv');
+    extensaoYY_tab = readtable('FEM_Decente/TensaoZ/extensaoYY.csv');
+    extensaoZZ_tab = readtable('FEM_Decente/TensaoZ/extensaoZZ.csv');
+    stressesAA_tab = readtable('FEM_Decente/TensaoZ/stressesAA.csv');
+
+    energy = table2array(energy_tab(:,2));
+    energy_density = table2array(energy_density_tab(:,2));
+    extensaoXX = table2array(extensaoXX_tab(:,2));
+    extensaoYY = table2array(extensaoYY_tab(:,2));
+    extensaoZZ = table2array(extensaoZZ_tab(:,2));
+    stressesAA = table2array(stressesAA_tab(:,2));
 
 end
